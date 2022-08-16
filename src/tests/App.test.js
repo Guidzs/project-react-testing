1d7bb5f343bd22ccff92c9e6e0a657bb255b2fae
Guidzs <guidz.2004@gmail.com>
2022-08-16 17:00:27 -0300

    requisito 1 - OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
@@ -13,5 +14,40 @@ describe('Teste o componente <App.js />', () => {
     expect(linkAbout).toBeInTheDocument();
     const linkFavoritePokémons = screen.getByRole('link', { name: /favorite pokémons/i });
     expect(linkFavoritePokémons).toBeInTheDocument();
+  });
+
+  test('se a aplicação é redirecionada para a URL / ao clicar em Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /home/i });
+
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('se a aplicação é redirecionada para a URL /about ao clicar em About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('se a aplicação é redirecionada para URL /favorites ao clicar em favorite', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavoritePokémons = screen.getByRole('link', { name: /favorite pokémons/i });
+
+    userEvent.click(linkFavoritePokémons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('se a aplicação renderiza a página Not Found em caso de URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/tyrbe');
+    const notFoundTitle = screen.getByRole('heading', { level: 2, name: /not found/i });
+    expect(notFoundTitle).toBeInTheDocument();
   });
 });
