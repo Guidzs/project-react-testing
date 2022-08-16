742c03295b2adb7af7cc9332424b313f7378cc88
Guidzs <guidz.2004@gmail.com>
2022-08-16 16:09:36 -0300

    verifica se os links estão na tela

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-describe('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  test('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    const linkFavoritePokémons = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkFavoritePokémons).toBeInTheDocument();
+  });
+});
